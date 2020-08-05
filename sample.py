4ad237e67dbfdb40623c9e9e56228f4a48150984
seungjun <mickymicmouse@gmail.com>
2020-08-05 17:17:56 +0900

    distribution

--- sample.py
+++ sample.py
@@ -10,25 +10,30 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 
-path = r"E:\\replaceMNIST"
+path = r"E:\\replaceMNIST_2"
+dis=['training', 'testing']
+y=open('log.txt',mode='w',encoding='euc-kr')
 
-for i in range(len(os.listdir(path))):
-    in_path = os.path.join(path, str(i))
-    file_name = "class_"+str(i)+".png"
-    image_list=os.listdir(in_path)
-
-    height=[]
-    for i in range(10):
-        a=cv2.imread(os.path.join(in_path, image_list[np.random.randint(0,len(image_list))]))
-        b=cv2.imread(os.path.join(in_path, image_list[np.random.randint(0,len(image_list))]))
-        c=cv2.imread(os.path.join(in_path, image_list[np.random.randint(0,len(image_list))]))
-        d=cv2.imread(os.path.join(in_path, image_list[np.random.randint(0,len(image_list))]))
-        e=cv2.imread(os.path.join(in_path, image_list[np.random.randint(0,len(image_list))]))
-        f=cv2.imread(os.path.join(in_path, image_list[np.random.randint(0,len(image_list))]))
-        
-        first=np.concatenate([a,b,c,d,e,f],axis=1)
-        height.append(first)
+for k in dis:
     
-    final = np.concatenate([height[x] for x in range(10)],axis=0)
-    plt.imshow(final)
-    cv2.imwrite(file_name, final)
+    for i in range(len(os.listdir(os.path.join(path, k)))):
+        in_path = os.path.join(path, k ,str(i))
+        file_name = k+"_class_"+str(i)+".png"
+        image_list=os.listdir(in_path)
+        y.write(k+"_class_"+str(i)+": "+ str(len(image_list)))
+        height=[]
+        for j in range(10):
+            a=cv2.imread(os.path.join(in_path, image_list[np.random.randint(0,len(image_list))]))
+            b=cv2.imread(os.path.join(in_path, image_list[np.random.randint(0,len(image_list))]))
+            c=cv2.imread(os.path.join(in_path, image_list[np.random.randint(0,len(image_list))]))
+            d=cv2.imread(os.path.join(in_path, image_list[np.random.randint(0,len(image_list))]))
+            e=cv2.imread(os.path.join(in_path, image_list[np.random.randint(0,len(image_list))]))
+            f=cv2.imread(os.path.join(in_path, image_list[np.random.randint(0,len(image_list))]))
+            
+            first=np.concatenate([a,b,c,d,e,f],axis=1)
+            height.append(first)
+        
+        final = np.concatenate([height[x] for x in range(10)],axis=0)
+        plt.imshow(final)
+        cv2.imwrite(file_name, final)
+y.close()
